ca4774f6b399e39cff5db2005bc4b6046312d57b
Michael Young <mryoung13@yahoo.com>
2019-12-11 13:36:21 -0800

    Updated MainViewModel

--- Young.PlanChecker/ViewModels/MainViewModel.cs
+++ Young.PlanChecker/ViewModels/MainViewModel.cs
@@ -1,7 +1,18 @@
-﻿using ESAPIX.Common;
+﻿using ESAPIX.Interfaces;
 using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESAPIX.Common;
 using VMS.TPS.Common.Model.API;
+using Prism.Commands;
+using System.Windows;
+using F = ESAPIX.Facade.API;
+using ESAPIX.Extensions;
+using ESAPIX.Constraints.DVH;
+using System.Collections.ObjectModel;
 
 namespace Young.PlanChecker.ViewModels
 {
@@ -11,9 +22,44 @@ namespace Young.PlanChecker.ViewModels
 
         public MainViewModel()
         {
-            
+            EvaluateCommand = new DelegateCommand(() =>
+            {
+                foreach (var pc in Constraints)
+                {
+                    var result = VMS.GetValue(sc =>
+                    {
+                        //Check if we can constrain first
+                        var canConstrain = pc.Constraint.CanConstrain(sc.PlanSetup);
+                        //If not..report why
+                        if (!canConstrain.IsSuccess) { return canConstrain; }
+                        else
+                        {
+                            //Can constrain - so do it
+                            return pc.Constraint.Constrain(sc.PlanSetup);
+                        }
+                    });
+                    //Update UI
+                    pc.Result = result;
+                }
+            });
+
+            CreateConstraints();
         }
 
-        
+        private void CreateConstraints()
+        {
+            Constraints.AddRange(new PlanConstraint[]
+            {
+                new PlanConstraint(ConstraintBuilder.Build("PTV", "Max[%] <= 110")),
+                new PlanConstraint(ConstraintBuilder.Build("Rectum", "V75Gy[%] <= 15")),
+                new PlanConstraint(ConstraintBuilder.Build("Rectum", "V65Gy[%] <= 35")),
+                new PlanConstraint(ConstraintBuilder.Build("Bladder", "V80Gy[%] <= 15")),
+                new PlanConstraint(new CTDateConstraint())
+            });
+        }
+
+
+        public DelegateCommand EvaluateCommand { get; set; }
+        public ObservableCollection<PlanConstraint> Constraints { get; private set; } = new ObservableCollection<PlanConstraint>();
     }
 }
